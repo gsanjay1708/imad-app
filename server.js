005db9ceba8c081affbb69e9a227fc791d09b3e1
gsanjay1708 <30703377+gsanjay1708@users.noreply.github.com>
2017-08-06 00:50:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one', function (req, res) {
-    res.sendFile('This is article one');
+    res.send('This is article one');
 });
 
 app.get('/article_two', function (req, res) {
